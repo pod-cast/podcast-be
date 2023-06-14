fd1f8dfdb13cf119bc1e6db68ae0701da458a990
azadvt <azad.vt@outlook.com>
2023-06-14 21:45:11 +0530

    init(server):nest.js server setup

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup() {
+    return 'signup';
+  }
+
+  @Post('signIn')
+  signIn() {
+    return 'signIn';
+  }
+}
